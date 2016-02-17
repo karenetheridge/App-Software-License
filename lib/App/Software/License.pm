290397fe6831cfafa50026bc1b188af6f3b2bfda
Karen Etheridge <ether@cpan.org>
2016-02-16 19:05:03 -0800

    increment $VERSION after 0.06 release

--- lib/App/Software/License.pm
+++ lib/App/Software/License.pm
@@ -2,7 +2,7 @@ package App::Software::License;
 # ABSTRACT: Command-line interface to Software::License
 # KEYWORDS: license licence LICENSE generate distribution build tool
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 use Moo;
 use MooX::Options;
